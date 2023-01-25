676217c71754c33b4c4c8e9831d3614976a22e07
Michael Scholl <michael.scholl@sysdig.com>
2023-01-25 15:31:16 -0800

    Updating chart to include namespace

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,92 +1,20 @@
-/*
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main_vpc"
-  }
+
+resource "random_id" "sko_bucket" {
+  byte_length = 8
 }
 
-resource "aws_subnet" "main_public_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
-
-  tags = {
-    Name = "main_public_1"
-  }
+resource "aws_s3_bucket" "sko_bucket" {
+  bucket = "sysdig-sko-bucket-${random_id.sko_bucket.hex}"
 }
 
-resource "aws_subnet" "main_public_2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_s3_bucket_public_access_block" "sko_bucket" {
+  bucket = aws_s3_bucket.sko_bucket.id
 
-  tags = {
-    Name = "main_public_2"
-  }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
-
-resource "aws_subnet" "main_public_3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "main_public_3"
-  }
-}
-
-resource "aws_subnet" "main_private_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.64.0/24"
-
-  tags = {
-    Name = "main_private_1"
-  }
-}
-
-resource "aws_subnet" "main_private_2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.65.0/24"
-
-  tags = {
-    Name = "main_private_2"
-  }
-}
-
-resource "aws_subnet" "main_private_3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.66.0/24"
-
-  tags = {
-    Name = "main_private_3"
-  }
-}
-
-resource "aws_subnet" "main_database_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.128.0/24"
-
-  tags = {
-    Name = "main_database_1"
-  }
-}
-
-resource "aws_subnet" "main_database_2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.129.0/24"
-
-  tags = {
-    Name = "main_database_2"
-  }
-}
-
-resource "aws_subnet" "main_database_3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.130.0/24"
-
-  tags = {
-    Name = "main_database_3"
-  }
-}
-*/
 
 resource "aws_security_group" "sko_compliance_violation" {
   vpc_id      = module.vpc.vpc_id
